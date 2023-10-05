6e3fdf96c8ea3b3e94acd59a6b49a00d7d25a2e9
DylanDeSchryver <142050322+DylanDeSchryver@users.noreply.github.com>
2023-10-05 07:44:20 -0400

    Update robot.py

--- robot.py
+++ robot.py
@@ -1,38 +1,44 @@
-import OPi.GPIO as GPIO
-import time
-
-
-class Tank:
+class RobotClass:
     def __init__(self):
         # Configure GPIO pins for motor control
-        self.motor1_forward_pin = 'PA8'  # Adjust GPIO pin numbers as needed
-        self.motor1_backward_pin = 'PA9'
-        self.motor2_forward_pin = 'PA10'
-        self.motor2_backward_pin = 'PA20'
+        self.motor1_enable_pin = 'PA8'  # PWM pin for motor 1
+        self.motor1_forward_pin = 'PA9'
+        self.motor1_backward_pin = 'PA10'
+        self.motor2_enable_pin = 'PA20'  # PWM pin for motor 2
+        self.motor2_forward_pin = 'PA21'
+        self.motor2_backward_pin = 'PA22'
 
-        GPIO.setmode(GPIO.SUNXI)
-
+        GPIO.setmode(GPIO.Sunxi)
         GPIO.setup(self.motor1_forward_pin, GPIO.OUT)
         GPIO.setup(self.motor1_backward_pin, GPIO.OUT)
         GPIO.setup(self.motor2_forward_pin, GPIO.OUT)
         GPIO.setup(self.motor2_backward_pin, GPIO.OUT)
 
-        self.motor1f = GPIO.PWM(self.motor1_forward_pin, 1000)  # 1000 Hz frequency
-        self.motor1b = GPIO.PWM(self.motor2_forward_pin, 1000)
-        self.motor2f = GPIO.PWM(self.motor1_backward_pin, 1000)  # 1000 Hz frequency
-        self.motor2b = GPIO.PWM(self.motor2_backward_pin, 1000)
+        # Initialize PWM pins for motor speed control
+        GPIO.setup(self.motor1_enable_pin, GPIO.OUT)
+        GPIO.setup(self.motor2_enable_pin, GPIO.OUT)
+        self.motor1_pwm = GPIO.PWM(self.motor1_enable_pin, 1000)  # 1000 Hz frequency
+        self.motor2_pwm = GPIO.PWM(self.motor2_enable_pin, 1000)
 
-        self.motor1f.start(50)
-        self.motor2f.start(50)
-        self.motor1b.start(50)
-        self.motor2b.start(50)
+        # Start PWM with 0% duty cycle (motors stopped)
+        self.motor1_pwm.start(0)
+        self.motor2_pwm.start(0)
 
-    def MOVEFWD(self):
-        print('beofre')
+    def MOVEFWD(self, speed):
+        # Set the direction pins for forward motion
         GPIO.output(self.motor1_forward_pin, GPIO.HIGH)
-        print("after")
         GPIO.output(self.motor2_forward_pin, GPIO.HIGH)
-        print("Hello WOrld")
+        GPIO.output(self.motor1_backward_pin, GPIO.LOW)
+        GPIO.output(self.motor2_backward_pin, GPIO.LOW)
+
+        # Set the speed for forward motion (0 to 100%)
+        self.SETSPEED(speed)
+
+    def SETSPEED(self, speed):
+        # Set motor speed using PWM (speed: 0 to 100)
+        self.motor1_pwm.ChangeDutyCycle(speed)
+        self.motor2_pwm.ChangeDutyCycle(speed)
+        print("Hello Daneil")
 
     def MOVEBACKWARD(self):
         GPIO.output(self.motor1_forward_pin, GPIO.LOW)
@@ -57,19 +63,16 @@ class Tank:
         GPIO.output(self.motor2_forward_pin, GPIO.LOW)
         GPIO.output(self.motor1_backward_pin, GPIO.LOW)
         GPIO.output(self.motor2_backward_pin, GPIO.LOW)
-        self.motor1f.ChangeDutyCycle(0)  # Stop PWM
-        self.motor2f.ChangeDutyCycle(0)
-        self.motor1b.ChangeDutyCycle(0)  # Stop PWM
-        self.motor2b.ChangeDutyCycle(0)
+        self.motor1_pwm.ChangeDutyCycle(0)  # Stop PWM
+        self.motor2_pwm.ChangeDutyCycle(0)
 
-    def SETSPEED(self, speed):
-        self.motor1f.ChangeDutyCycle(speed)
-        self.motor2f.ChangeDutyCycle(speed)
-        print("Hello Daniel")
+
+
+
 
     def cleanup(self):
+        # Clean up GPIO settings and PWM
         GPIO.cleanup()
-        self.motor1f.stop()
-        self.motor2f.stop()
-        self.motor1b.stop()
-        self.motor2b.stop()
+        self.motor1_pwm.stop()
+        self.motor2_pwm.stop()
+
